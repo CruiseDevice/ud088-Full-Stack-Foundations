0094f7a934ab5762d8bf711360ec4113efd507fb
Akash Chavan <meakashchavan@gmail.com>
2017-05-09 21:21:19 +0530

    Objective-3 complete

--- Lesson 2/Objective-1/webserver.py
+++ Lesson 2/Objective-1/webserver.py
@@ -64,7 +64,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
-                output += "<html><body>List all the restaurants names in the database"
+                output += "<html><body><a href = '/restaurants/new'>Make a New Restaurant Here</a><br/><br/>List all the restaurants names in the database"
                 # Query all the restaurants and return the results in any order
                 result = session.query(Restaurant).all()
                 print result
@@ -81,27 +81,50 @@ class webServerHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                output = ""
+                output += "<html>"
+                output += "<body>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += '''<form method='POST' enctype = 'multipart/form-data' action='/restaurants/new'><input name="restaurant-name" type="text" placeholder = "New Restaurant Name" ><input type="submit" value="Create"> </form>'''
+                output += "</body>"
+                output += "</html>"
+                self.wfile.write(output)
+                return
+
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                # self.end_headers()
+                ctype,pdict = cgi.parse_header(
+                    self.headers.getheader('Content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile,pdict)
+                    messagecontent = fields.get('restaurant-name')
+
+                    # create a new Restaurant Object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_header('Location','/restaurants')
+                    self.end_headers()
+                # output = ""
+                # output += "<html><body>"
+                # output += " <h2> Okay, how about this: </h2>"
+                # output += "<h1> %s </h1>" % messagecontent[0]
+                # output += '''<form method='POST' enctype = 'multipart/form-data' action='/restaurant/new'><input name="restaurant-name" type="text" placeholder = "New Restaurant Name"><input type="submit" value="Create"> </form>'''
+                # output += "</body></html>"
+                self.wfile.write(output)
+                print output
         except:
             pass
 
