272cf292ae18806e488a85ca0cccd5e8bdf720e5
Akash Chavan <meakashchavan@gmail.com>
2017-05-12 02:14:17 +0530

    Objective-5 complete.

--- Lesson 2/Objectives/webserver.py
+++ Lesson 2/Objectives/webserver.py
@@ -74,7 +74,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                     # print item[0]
                     output += item.name
                     output += "<br/><a href = '/restaurants/%s/edit'>Edit</a><br/>" % item.id
-                    output += "<a href = '#'>Delete</a></br/>"
+                    output += "<a href = '/restaurants/%s/delete'>Delete</a></br/>" % item.id
                     output += "<br/><br/><br/>"
 
                 output += "</body></html>"
@@ -116,6 +116,29 @@ class webServerHandler(BaseHTTPRequestHandler):
 
                     self.wfile.write(output)
 
+
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+
+                    output = "<html><body>"
+                    output += "<h1>Are you sure you want to delete "
+                    output += myRestaurantQuery.name
+                    output += "?</h1>"
+                    output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/delete' >" % restaurantIDPath
+                    # output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                    output += "<input type = 'submit' value = 'Delete'>"
+                    output += "</form>"
+                    output += "</body></html>"
+
+                    self.wfile.write(output)
+
+
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
@@ -134,6 +157,25 @@ class webServerHandler(BaseHTTPRequestHandler):
                     if myRestaurantQuery != []:
                         myRestaurantQuery.name = messagecontent[0]
                         session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+
+            if self.path.endswith("/delete"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    # messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+
+                    myRestaurantQuery = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        # myRestaurantQuery.name = messagecontent[0]
+                        session.delete(myRestaurantQuery)
                         session.commit()
                         self.send_response(301)
                         self.send_header('Content-type', 'text/html')
